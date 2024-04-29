5b4a129e71f06982ed8b370984dcf11f77852336
Yamini Punyavathi Muttevi <m.yamini91@gmail.com>
2024-04-29 17:06:04 +0530

    Updated the search
    Updated the search  for exceptions.

--- react-algolia/src/App.jsx
+++ react-algolia/src/App.jsx
@@ -1,27 +1,43 @@
 import './App.css'
 import { getPostsData } from './algolia-client'
 import {Post } from './Post'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+
 
 function App() {
+  const [searchValue, setSearchValue] = useState('')
+  const onSearchChange = (e) => {
+    setSearchValue(e.target.value)
+  }
+
   const [loading, setLoading] = useState(false)
   const [posts, setPosts] = useState()
   useEffect(() => {
     const handler = async () => {
       setLoading(true)
-      const data = await getPostsData('')
+      const data = await getPostsData(searchValue)
       if (data) setPosts(data.posts)
       setLoading(false)
     }
     handler()
-  }, [])
+  }, [searchValue])
+
+
 
   return (
     <main>
       <h1 className="page-title">Exceptions</h1>
+      <input
+        type="text"
+        className="posts-search"
+        placeholder="Type your search here"
+        value={searchValue}
+        onChange={onSearchChange}
+    
+      />
       <section className="posts">
         {!posts?.length && <p className="state-message">{loading ? 'Fetching posts...' : 'No results!'}</p>}
-        {!!posts?.length && posts.map((post) => <Post post={post} key={post.objectID} />)}
+        {!!posts?.length && posts.map((post) =>  <div className="how-section1"><Post post={post} key={post.objectID} ></Post> </div>  )}
       </section>
     </main>
   )
--- react-algolia/src/Post.jsx
+++ react-algolia/src/Post.jsx
@@ -4,9 +4,23 @@ export const Post = ({ post}) => {
   
   
     return (
-       <div className="post-card">
-      <span className="post-card-category">{post.fields.exception['en-US']}</span>
-      <span className="post-card-title">{post.fields.resolution['en-US']}</span>
-     
-    </div>
+  
+      <div className="row">
+          <div className="col-md-6 how-img">
+        
+              <img src="error.png" className="rounded-circle img-fluid" alt=""/>
+          </div>
+          <div className="col-md-6">
+              <h4></h4>
+
+          <span className="summy-span">Exception:</span> <h5 className="heading">{post.fields.exception['en-US']}</h5>
+       <span className="summy-span" >Resolution: </span><h5 className="subheading">{post.fields.resolution['en-US']}</h5>
+       <span className="summy-span" >Summary:</span> <p className="text-muted">Freedom to work on ideal projects. On GetLance, you run your own business and choose your own clients and projects. Just complete your profile and we’ll highlight ideal jobs. Also search projects, and respond to client invitations.</p>
+          </div>
+      </div>
+   
+
  ) }
+ 
+
+ 
